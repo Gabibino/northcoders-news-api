4f5ad45f9155b686d22f9d730a6f09f68c528732
Gabriela <gabrielabinobr@gmail.com>
2024-11-26 13:13:51 +0000

    4-get-article-by-id

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,5 +1,6 @@
 const endpointsJson = require("../endpoints.json");
-const {getAllTopics} = require ('../models/api.models')
+const {getAllTopics} = require ('../models/api.models');
+const { getArticleById } = require('../models/api.models');  
 
 function getApi(req, res) {
     console.log(endpointsJson, "<--- endpoints json");
@@ -15,5 +16,22 @@ function getApiTopics (req, res){
     });
 }
 
+function getApiArticleById(req, res) {
+    const { article_id } = req.params; // Pega o article_id da URL
 
-module.exports = {getApi, getApiTopics};
+    //checking if article_id is valid (if it is a number as an exemple)
+    if (isNaN(article_id)) {
+        return res.status(400).send({ error: "Invalid article ID" });
+    }
+
+    getArticleById(Number(article_id)) //converting it to a number in case is not
+        .then((article) => {
+            if (!article) {
+                return res.status(404).send({ error: "Article not found" });
+            }
+            res.status(200).send(article); //send the article found
+        });
+}
+
+
+module.exports = {getApi, getApiTopics, getApiArticleById};
